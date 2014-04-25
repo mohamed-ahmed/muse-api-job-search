a8f88558e91b5ac7fd8a1ff0cab0ccd545168e96
Mohamed Ahmed <mohamed.ahmed@live.ca>
2014-04-24 21:33:33 -0400

    web server rendering template

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from tornado import gen
 from tornado.options import define, options, parse_command_line, parse_config_file
 from tornado.web import Application, RequestHandler, authenticated
 from tornado.httpclient import AsyncHTTPClient
+import os.path
 
 define('port', default=7777, help="port to listen on")
 
@@ -14,20 +15,31 @@ class GenAsyncHandler(RequestHandler):
     @gen.coroutine
     def get(self):
         http_client = AsyncHTTPClient()
-        response = yield http_client.fetch("https://www.themuse.com/api/v1/jobs?page=0&company=Artsicle&job_category=Engineering&job_level=Internship&job_location=New+York%2C+NY")
+        #response = yield http_client.fetch("https://www.themuse.com/api/v1/jobs?page=0&company=Artsicle&job_category=Engineering&job_level=Internship&job_location=New+York%2C+NY")
         
         #print(response.body)
-        jsonResponse = json_decode(response.body)
-        print(jsonResponse["results"])
-        # self.render("template.html")
+        #jsonResponse = json_decode(response.body)
+        #print(jsonResponse["results"])
+        self.render("index.html")
+
+
+
+class MyApplication(Application):
+    def __init__(self):
+        handlers = [('/', GenAsyncHandler),]
+
+        settings = dict(
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+
+        Application.__init__(self, handlers, **settings)
+
 
 def main():
-    app = Application(
-        [
-            ('/', GenAsyncHandler),
-
-        ])
-
+    app = MyApplication()
     app.listen(options.port)
     logging.info('Listening on http://localhost:%d' % options.port)
     IOLoop.instance().start()
